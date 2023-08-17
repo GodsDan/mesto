f48b1d416343b2aa3a765b51b086e9a75d0ecbc7
GodsDan <gods.ggiven@gmail.com>
2023-08-17 21:36:30 +0300

    error correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,39 @@
+// Создаем переменные
+let profileButtonEdit = document.querySelector('.profile__button-edit');// кнопка изменения данных профиля
+let profileButtonAdd = document.querySelector('.profile__button-add'); // кнопка добавления новой карточки 
+
+let popupSectionEditProfile = document.getElementById ('popup-profile');//попап изменения данных профиля
+let popupSectionAddCard = document.getElementById ('popup-card');//попап добавления новой карточки
+let popupSectionOpenImage = document.getElementById ('popup-increase-image'); //попап открытия картинки
+
+let popupCloseButtonEdit = popupSectionEditProfile.querySelector('.popup__close-button');//кнопка закрытия попапа редактирования информации профиля
+let popupCloseButtonAdd = popupSectionAddCard.querySelector('.popup__close-button'); //кнопка закрытия попапа добавления новой карточки
+let popupCloseButtonOpenImage = popupSectionOpenImage.querySelector('.popup__close-button'); //кнопка закрытия картинки
+
+let profileName = document.querySelector('.profile__name'); // находим поле для вывода имени на странице
+let profileDescription = document.querySelector('.profile__description'); // находим поле "о себе" на странице
+
+// Находим форму изменения данных профиля в DOM
+let formElementEditProfile = popupSectionEditProfile.querySelector('.form');
+// Находим форму добавления новой карточки в DOM
+let formElementAddCard = popupSectionAddCard.querySelector('.form');
+
+// Находим поля формы в DOM
+let nameInput = formElementEditProfile.querySelector('.form__data_input_name');// Имя 
+let jobInput = formElementEditProfile.querySelector('.form__data_input_description'); // О себе
+let locationInput = formElementAddCard.querySelector('.form__data_input_location'); // Название для карточки
+let linkInput = formElementAddCard.querySelector('.form__data_input_link'); // Ссылка на картинку
+
+
+
 // проходим по массиву и выводим в консоль
   initialCards.forEach(function(elem){
     let element = document.querySelector('.elements');
     let elementTemplate = document.querySelector('#element').content;
     let cardElement = elementTemplate.cloneNode(true);
     cardElement.querySelector('.element__image').src = elem.link;//ссылка
-    cardElement.querySelector('.element__title').textContent = elem.name;//добавили текст и альт
-    cardElement.querySelector('.element__image').alt = elem.name;
+    cardElement.querySelector('.element__title').textContent = elem.name;//добавили текст
+    cardElement.querySelector('.element__image').alt = elem.name; //добавили альт 
     cardElement.querySelector('.element__like').addEventListener('click', function(evt) {
         evt.target.classList.toggle('element__like_active');
     });
@@ -14,7 +42,7 @@
     element.append(cardElement);  //добавить карточки в конец
 });
 
-function openImage (evt) {
+function openImage (evt) { //фунция для увеличения картинки
     let imageElement = evt.target;
     let imageSrc = imageElement.src;
     let imageTitle = imageElement.alt;
@@ -24,12 +52,10 @@ function openImage (evt) {
     title.textContent = imageTitle;
     popupSectionOpenImage.classList.add('popup_opened');
     popupSectionOpenImage.classList.add('popup_back');
-    console.log('На меня кликнули',imageSrc,imageTitle);
 }
 
 //Функция удаления карточки
 function selectElementItem (elem) {
-    console.log('elem', elem);
     let buttonDeleteElement = elem.querySelector('.element__trash');//кнопка удаления карточки
     buttonDeleteElement.addEventListener('click', elementDelete)
 }
@@ -38,8 +64,6 @@ function elementDelete(event){
     let item = event.target.closest('.element');
     item.remove();
 }
-
-
 
 
 // Создаем карточку
@@ -58,38 +82,6 @@ function newCard (){
     element.prepend(addCardElement);
     
 }
-
-
-// Создаем переменные
-let profileButtonEdit = document.querySelector('.profile__button-edit');// кнопка изменения данных профиля
-let profileButtonAdd = document.querySelector('.profile__button-add'); // кнопка добавления новой карточки 
-
-let popupSectionEditProfile = document.getElementById ('popup-profile');//попап изменения данных профиля
-let popupSectionAddCard = document.getElementById ('popup-card');//попап добавления новой карточки
-let popupSectionOpenImage = document.getElementById ('popup-increase-image'); //попап открытия картинки
-
-let popupCloseButtonEdit = popupSectionEditProfile.querySelector('.popup__close-button');//кнопка закрытия попапа редактирования информации профиля
-let popupCloseButtonAdd = popupSectionAddCard.querySelector('.popup__close-button'); //кнопка закрытия попапа добавления новой карточки
-let popupCloseButtonOpenImage = popupSectionOpenImage.querySelector('.popup__close-button');
-
-let profileName = document.querySelector('.profile__name'); // находим поле для вывода имени на странице
-let profileDescription = document.querySelector('.profile__description'); // находим поле "о себе" на странице
-
-
-
-
-// Находим форму изменения данных профиля в DOM
-let formElementEditProfile = popupSectionEditProfile.querySelector('.form');
-// Находим форму добавления новой карточки в DOM
-let formElementAddCard = popupSectionAddCard.querySelector('.form');
-
-// Находим поля формы в DOM
-let nameInput = formElementEditProfile.querySelector('.form__data_input_name');// Имя 
-let jobInput = formElementEditProfile.querySelector('.form__data_input_description'); // О себе
-let locationInput = formElementAddCard.querySelector('.form__data_input_location'); // Название для карточки
-let linkInput = formElementAddCard.querySelector('.form__data_input_link'); // Ссылка на картинку
-
-
 
 // изменение данных профиля
 profileButtonEdit.addEventListener('click',()=>{
@@ -110,7 +102,8 @@ function handleFormSubmitEditProfile (evt) {
     popupSectionEditProfile.classList.remove('popup_opened');
 }
 formElementEditProfile.addEventListener('submit', handleFormSubmitEditProfile);
-//////////////////////////////////////////////////////////////////////
+
+
 // Добавление новой карточки
 profileButtonAdd.addEventListener('click',()=>{
     popupSectionAddCard.classList.add('popup_opened');
